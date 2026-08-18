8b261f7c80519c4638d0ef1c414fffca27959f58
andrewrowanwallee <andrew.rowan@wallee.com>
2026-08-18 12:04:54 +0200

    Release 1.0.31

--- GXModules/PostFinanceCheckout/PostFinanceCheckoutPayment/Library/Helper/PostFinanceCheckoutHelper.php
+++ GXModules/PostFinanceCheckout/PostFinanceCheckoutPayment/Library/Helper/PostFinanceCheckoutHelper.php
@@ -48,7 +48,7 @@ class PostFinanceCheckoutHelper
 		$paymentMethodConfigurations = $paymentService->getPaymentMethodConfigurations();
 		foreach ($paymentMethodConfigurations as $paymentMethodConfiguration) {
 			$slug = 'postfinancecheckout_' . trim(strtolower(self::slugify($paymentMethodConfiguration->getName())));
-			if ($_SESSION['choosen_payment_method'] === $slug) {
+			if ($_SESSION['chosen_payment_method'] === $slug) {
 				$paymentMethodConfigurationId = $paymentMethodConfiguration->getId();
 				break;
 			}
--- GXModules/PostFinanceCheckout/PostFinanceCheckoutPayment/Shop/Templates/All/Javascript/extenders/postfinancecheckout-app.js
+++ GXModules/PostFinanceCheckout/PostFinanceCheckoutPayment/Shop/Templates/All/Javascript/extenders/postfinancecheckout-app.js
@@ -142,8 +142,11 @@
          * @param validationResult
          */
         validationCallBack: function (validationResult) {
+            const validationStatusField = document.querySelector(PostFinanceCheckoutCheckout.payment_method_handler_status);
             if (validationResult.success) {
-                document.querySelector(this.payment_method_handler_status).value = true;
+                if (validationStatusField) {
+                    validationStatusField.value = true;
+                }
                 PostFinanceCheckoutCheckout.handler.submit();
             } else {
                 document.body.scrollTop = 0;
@@ -152,7 +155,9 @@
                 if (validationResult.errors) {
                     PostFinanceCheckoutCheckout.showErrors(validationResult.errors);
                 }
-                document.querySelector(this.payment_method_handler_status).value = false;
+                if (validationStatusField) {
+                    validationStatusField.value = false;
+                }
                 PostFinanceCheckoutCheckout.activateLoader(false);
             }
         },
